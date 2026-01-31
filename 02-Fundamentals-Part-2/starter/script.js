39deb47d5a998e34feebea85d60ad2cc18451a8f
¨Carlos <carlos.smith.t@uni.pe>
2026-01-31 00:10:22 -0500

    strict-mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,20 @@
+// Usar modo strict
+
+/*
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+
+console.log(hasDriversLicense)
+
+const private = 535;
+
+const if = 23;
+
+// Ayuda a debugear, ni if ni private se pueden utilizar porque son declaraciones
+
+*/
+
