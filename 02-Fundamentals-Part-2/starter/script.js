83421ab66ccbbb1577bb6981c4849c365a6350d7
¨Carlos <carlos.smith.t@uni.pe>
2026-02-03 19:28:31 -0500

    assignment-func

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,8 @@
 // Usar modo strict
+'use strict';
 
 /*
-'use strict';
+
 
 let hasDriversLicense = false;
 const passTest = true;
@@ -16,5 +17,49 @@ const if = 23;
 
 // Ayuda a debugear, ni if ni private se pueden utilizar porque son declaraciones
 
-*/
+// 1. Declaración de la función (Definición)
+function logger() {
+    // Cuerpo de la función (Function Body)
+    console.log('Mi nombre es Carlos');
+}
 
+// 2. Invocación / Llamada / Ejecución ==== Se refieren a lo mismo
+logger(); // Se ejecuta el código de arriba
+logger(); // Se ejecuta otra vez
+logger(); // Y otra vez
+
+
+function fruitProcessor(apples, oranges) {
+    // 'apples' y 'oranges' son PARÁMETROS
+
+    // Procesamiento (Lógica)
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+
+    // Output (Devolver valor)
+    return juice;
+}
+
+// Llamada 1: Pasamos 5 y 0 como ARGUMENTOS
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// Salida: "Juice with 5 apples and 0 oranges."
+
+// Llamada 2: Reutilizamos la lógica con diferentes datos
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+// Salida: "Juice with 2 apples and 4 oranges."
+
+
+
+
+//Assignment - Function
+
+function describeCountry(country, population, capitalCity) {
+    return console.log(`${country} has ${population} million people and its capital city is ${capitalCity}`)
+}
+
+describeCountry("Peru", 34, "Lima")
+describeCountry("Chile", 11, "Santiago De Chile")
+describeCountry("Argentina", 54, "Buenos Aires")
+
+*/
